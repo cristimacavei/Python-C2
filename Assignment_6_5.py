e00026f3d60d1c9510c33f67fee850903613191b
Cristi Macavei <cristi_macavei06@yahoo.com>
2023-01-16 10:50:21 +0200

    Remove Old Assignment_6_5.py

--- Assignment_6_5.py
+++
@@ -1,8 +0,0 @@
-# Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below.
-# Convert the extracted value to a floating point number and print it out.
-
-text = "X-DSPAM-Confidence:    0.8475"
-x = text.find("0")
-print(x)
-y = slice(23,30)
-print(float(text[y]))
